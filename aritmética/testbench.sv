5f321fdb26d85a7d351e9b3eab1c3c06048cd7df
Anderson Moura <47828681+andersonmoura-cn@users.noreply.github.com>
2025-01-17 02:38:11 -0300

    nda

--- aritmética/testbench.sv
+++ aritmética/testbench.sv
@@ -1,25 +1,30 @@
-module test();
-  wire [3:0] sum, Quotient, Remainder;
-  wire [7:0] Product;
-  wire c_out;
-  controlador CONTROLADOR(.Sum(sum), .Carry(c_out), .Product(Product), .Quotient(Quotient), .Remainder(Remainder));
+module testbench();
+  reg clk;
+  wire [7:0] Sum, Quotient, Remainder;
+  wire [15:0] Product;
+  wire Carry;
 
+  // Instanciar o módulo controlador
+  controlador uut (
+    .clk(clk),
+    .Sum(Sum),
+    .Carry(Carry),
+    .Product(Product),
+    .Quotient(Quotient),
+    .Remainder(Remainder)
+  );
+
+  // Clock
   initial begin
-    monitor;
+    clk = 0;
+    forever #5 clk = ~clk;
   end
-  
-  task monitor;
-    integer i;
-    $monitor("Tempo:%0t -> Sum:%0b, Carry:%0b", $time, sum, c_out);
-    //$strobe("Tempo:%0t -> Sum:%0b, Carry:%0b", $time, sum, c_out);
-	$monitor("Tempo:%0t -> Quotient:%0b, Remainder:%0b", $time, Quotient, Remainder);
-    for (i = 0; i < 5; i = i + 1) begin
-      #10; // Espera para atualização de A, B e SUB
-      if (Product !== 8'bxxxxxxxx) begin
-        $display("Tempo:%0t -> Product:%0b", $time, Product);
-      end
-    end
-    $finish; 
-endtask
 
+  // Monitoramento
+  initial begin
+    $monitor("Time: %0t | Sum: %0b | Product: %0b | Quotient: %0b | Remainder: %0b | Carry: %0b", 
+         $time, Sum, Product, Quotient, Remainder, Carry);
+
+    #500 $finish;
+  end
 endmodule
